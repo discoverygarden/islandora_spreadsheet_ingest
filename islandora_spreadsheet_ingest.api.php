2151efb945d4a84dcd7395f246db65e502b0d74f
willtp87 <willtp87@hotmail.com>
2017-11-17 15:05:03 -0400

    wip

--- islandora_spreadsheet_ingest.api.php
+++ islandora_spreadsheet_ingest.api.php
@@ -4,3 +4,19 @@
  * @file
  * Module API documentation.
  */
+
+/**
+ * This hook allows modules to provide templates.
+ *
+ * @return array
+ *   An associative array mapping an id to an array with the keys 'name' and
+ *   'uri' containing a human readable name and URI for the template.
+ */
+function hook_islandora_spreadsheet_ingest_templates() {
+  return array(
+    'my_template_id' => array(
+      'name' => 'my awesome template',
+      'uri' => 'uri/to_my/template',
+    ),
+  );
+}
