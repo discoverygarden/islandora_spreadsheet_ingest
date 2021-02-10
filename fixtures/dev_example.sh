34ee4183785e6bf3880a9c28a1f7bc0d79bc2608
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-02-10 17:29:00 -0400

    Test stuff together.

--- fixtures/dev_example.sh
+++ fixtures/dev_example.sh
@@ -2,7 +2,7 @@
 
 MODULE=islandora_spreadsheet_ingest
 MG=isi
-MIGRATIONS=(${drush migrate:status --group=$MG --field=id})
+MIGRATIONS=($(drush migrate:status --group=$MG --field=id))
 APACHE_USER=www-data
 
 function reset_all() {
@@ -11,19 +11,21 @@ function reset_all() {
   done
 }
 
-DEST_LINK="${drush drupal:directory files}/isifixturefiles"
-FIXTURES="${drush drupal:directory $MODULE}/fixtures"
+DEST_LINK="$(drush drupal:directory files)/isifixturefiles"
+FIXTURES="$(drush drupal:directory $MODULE)/fixtures"
 
-sudo -u $APACHE_USER -- ln -s -f $FIXTURES $DEST_LINK && \
+sudo -u $APACHE_USER -- rsync -av $FIXTURES $DEST_LINK && \
 # Cheap script to facilitate dev.
 reset_all && \
 # Known issue with content sync causes OOM. Disable the context config.
+drush -n config:set migrate_plus.migration_group.isi shared_configuration.source.file "$FIXTURES/migration_example.csv" && \
+drush cr && \
 drush migrate:rollback -v --group=$MG && \
 drush pm-uninstall $MODULE && \
 drush en $MODULE && \
-drush config:set migrate_plus.migration_group.isi shared_configuration.source.file "$FIXTURES/migration_example.csv" && \
+drush -n config:set migrate_plus.migration_group.isi shared_configuration.source.file "$FIXTURES/migration_example.csv" && \
 drush cr && \
-drush-test-no-empty $MODULE && \
+#drush-test-no-empty $MODULE && \
 reset_all && \
 # Update user as needed.
 sudo -u $APACHE_USER -- drush migrate:batch-import -u 1 -v --uri=http://localhost --execute-dependencies --group=$MG ;\
