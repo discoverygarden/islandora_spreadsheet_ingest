39b17984fad2edb3b89a940c2f37cf06feaa0620
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-02-10 16:14:10 -0400

    Adjust fixtures.

--- fixtures/dev_example.sh
+++ fixtures/dev_example.sh
@@ -1,21 +1,33 @@
+#!/bin/bash
+
+MODULE=islandora_spreadsheet_ingest
+MG=isi
+MIGRATIONS=(${drush migrate:status --group=$MG --field=id})
+APACHE_USER=www-data
+
+function reset_all() {
+  for MIG in "${MIGRATIONS[@]}"; do
+    drush migrate:reset-status $MIG || return 2
+  done
+}
+
+DEST_LINK="${drush drupal:directory files}/isifixturefiles"
+FIXTURES="${drush drupal:directory $MODULE}/fixtures"
+
+sudo -u $APACHE_USER -- ln -s -f $FIXTURES $DEST_LINK && \
 # Cheap script to facilitate dev.
-drush migrate-reset-status islandora_spreadsheet_nodes_example &&\
-drush migrate-reset-status islandora_spreadsheet_files_example &&\
-drush migrate-reset-status islandora_spreadsheet_media_example &&\
+reset_all && \
 # Known issue with content sync causes OOM. Disable the context config.
-drush migrate-rollback -v --group=islandora_spreadsheet_example &&\
-drush pm-uninstall islandora_spreadsheet_ingest &&\
-drush en islandora_spreadsheet_ingest &&\
-drush cr &&\
-drush-test-no-empty islandora_spreadsheet_ingest &&\
-drush migrate-reset-status islandora_spreadsheet_nodes_example &&\
-drush migrate-reset-status islandora_spreadsheet_files_example &&\
-drush migrate-reset-status islandora_spreadsheet_media_example &&\
+drush migrate:rollback -v --group=$MG && \
+drush pm-uninstall $MODULE && \
+drush en $MODULE && \
+drush config:set migrate_plus.migration_group.isi shared_configuration.source.file "$FIXTURES/migration_example.csv" && \
+drush cr && \
+drush-test-no-empty $MODULE && \
+reset_all && \
 # Update user as needed.
-sudo -u www-data drush migrate:batch-import -u 1 -v --uri=http://localhost --execute-dependencies --group=islandora_spreadsheet_example ;\
-echo Files &&\
-drush migrate-messages islandora_spreadsheet_files_example &&\
-echo Nodes &&\
-drush migrate-messages islandora_spreadsheet_nodes_example &&\
-echo Media &&\
-drush migrate-messages islandora_spreadsheet_media_example
+sudo -u $APACHE_USER -- drush migrate:batch-import -u 1 -v --uri=http://localhost --execute-dependencies --group=$MG ;\
+for MIG in "${MIGRATIONS[@]}"; do
+  echo $MIG
+  drush migrate:messages $MIG
+done
