339b1922e8dc74495aafc1f79357d83f2bc90dae
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-05-26 12:15:13 -0300

    Filter the available ops.

--- src/Controller/RequestListBuilder.php
+++ src/Controller/RequestListBuilder.php
@@ -27,11 +27,26 @@ class RequestListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    $row['label'] = $entity->label();
+    $row['label'] = $entity->toLink(NULL, 'edit-form');
+    if (!$row['label']->getUrl()->access()) {
+      $row['label'] = $entity->label();
+    }
     $row['id'] = $entity->id();
     $row['active'] = $entity->getActive() ? $this->t('Active') : $this->t('Inactive');
 
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOperations(EntityInterface $entity) {
+    $ops = parent::getOperations($entity);
+
+    // Filter to only those operations to which the user has access.
+    return array_filter($ops, function ($op) {
+      return $op['url']->access();
+    });
   }
 
 }
