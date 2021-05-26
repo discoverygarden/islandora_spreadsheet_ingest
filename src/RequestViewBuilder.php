2731a5ff31c5a1c94a9b4f15649237fb2b4f4b63
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-05-26 11:39:01 -0300

    Throw together a little display.

--- src/RequestViewBuilder.php
+++ src/RequestViewBuilder.php
@@ -2,10 +2,69 @@
 
 namespace Drupal\islandora_spreadsheet_ingest;
 
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\EntityViewBuilder;
+use Drupal\Core\Render\Element;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * View builder for isi_request entities.
  */
 class RequestViewBuilder extends EntityViewBuilder {
+
+  /**
+   * User/account storage service.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $userStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildMultiple(array $build_list) {
+
+    foreach (Element::children($build_list) as $key) {
+      $item =& $build_list[$key];
+
+      $item['label'] = [
+        '#type' => 'item',
+        '#title' => $this->t('Label'),
+        '#markup' => $item['#isi_request']->label(),
+      ];
+
+      $owner = $this->userStorage->load($item['#isi_request']->getOwner());
+      $item['owner'] = [
+        '#type' => 'item',
+        '#title' => $this->t('Owner'),
+        'username' => ($owner ? [
+            '#theme' => 'username',
+            '#account' => $owner,
+          ] :
+          $this->t('Unknown user')),
+      ];
+      $item['active'] = [
+        '#type' => 'item',
+        '#title' => $this->t('Active'),
+        '#markup' => ($item['#isi_request']->getActive() ?
+          $this->t('Yes') :
+          $this->t('No')),
+      ];
+    }
+
+    return $build_list;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    $instance = parent::createInstance($container, $entity_type);
+
+    $instance->userStorage = $container->get('entity_type.manager')->getStorage('user');
+
+    return $instance;
+  }
+
 }
