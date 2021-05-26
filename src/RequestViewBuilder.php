18cee6e7c8e79b433f9957deab8b17fc86936133
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-05-26 10:45:10 -0300

    More granular permissions.

---
+++ src/RequestViewBuilder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\islandora_spreadsheet_ingest;
+
+use Drupal\Core\Entity\EntityViewBuilder;
+
+/**
+ * View builder for isi_request entities.
+ */
+class RequestViewBuilder extends EntityViewBuilder {
+}
