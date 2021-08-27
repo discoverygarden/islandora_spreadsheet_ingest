6de00b85050e185e3e5b511510ee0291263cc5fb
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-08-27 16:19:58 -0300

    Trim subdelimited strings by default.

--- src/Plugin/migrate/process/SubDelimitedExplode.php
+++ src/Plugin/migrate/process/SubDelimitedExplode.php
@@ -35,6 +35,10 @@ use Drupal\migrate\Row;
  *   not used, the resultant subset arrays will be indexed from 0. If a subset
  *   array has more values than keys given, additional keys will be provided at
  *   the same native index in the subset.
+ * - trim: (optional) Flag indicating if whitespace should be trimmed from top-
+ *   level elemeents. Defaults to TRUE.
+ * - subtrim: (optional) Flag indicating if whitespace should be trimmed from
+ *   subelemeents. Defaults to TRUE.
  *
  * For example, given the following cell contents:
  *
@@ -95,44 +99,140 @@ use Drupal\migrate\Row;
 class SubDelimitedExplode extends ProcessPluginBase {
 
   /**
-   * {@inheritdoc}
+   * Corresponds to the 'limit' variable for explode.
+   *
+   * See the PHP user's manual for explode() for more details; the short
+   * version is that positive numbers limit the number of subdivisions you get
+   * (the last element being the rest of the string), and negative numbers
+   * remove that number of subdivisions from the end of the resultant list.
+   *
+   * @var int
    */
-  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+  protected $limit;
+
+  /**
+   * The same as 'limit', but for individual items inside each element.
+   *
+   * Each element can have items up to the one given sublimit.
+   *
+   * @var int
+   */
+  protected $sublimit;
+
+  /**
+   * An array of the names of keys to apply to subsets, in order.
+   *
+   * Use this to tie the structure of the subset to the keys needed by the
+   * destination field. If not used, the resultant subset arrays will be indexed
+   * from 0. If a subset array has more values than keys given, additional keys
+   * will be provided at the same native index in the subset.
+   *
+   * @var array
+   */
+  protected $keys;
+
+  /**
+   * The boundary string for top-level elements.
+   *
+   * @var string
+   */
+  protected $delimiter;
+
+  /**
+   * The boundary string for individual items inside each element.
+   *
+   * @var string
+   */
+  protected $sublimiter;
+
+  /**
+   * Indicate if each top-level element should have its space trimmed.
+   *
+   * @var bool
+   */
+  protected $trim;
+
+  /**
+   * Indicate if each subelement should have its space trimmed.
+   *
+   * @var bool
+   */
+  protected $subtrim;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
     // Run some tests on the data.
     if (empty($this->configuration['delimiter']) || empty($this->configuration['subdelimiter'])) {
       throw new MigrateException("The 'delimiter' and 'subdelimiter' properties must both be provided for subdelimited_explode processes.");
     }
-    if (!is_string($value)) {
-      throw new MigrateException(sprintf('The value provided to subdelimited_explosion must be a string; actual provided value: %s', var_export($value, TRUE)));
-    }
     if (!empty($this->configuration['keys']) && !is_array($this->configuration['keys'])) {
       throw new MigrateException("The list of 'keys' provided should be an array.");
+    }
+
+    // Establish some variables.
+    $this->limit = $this->configuration['limit'] ?? PHP_INT_MAX;
+    $this->sublimit = $this->configuration['sublimit'] ?? PHP_INT_MAX;
+    $this->keys = $this->configuration['keys'] ?? [];
+    $this->delimiter = $this->configuration['delimiter'];
+    $this->subdelimiter = $this->configuration['subdelimiter'];
+    $this->trim = $this->configuration['trim'] ?? TRUE;
+    $this->subtrim = $this->configuration['subtrim'] ?? TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    // Run some tests on the data.
+    if (!is_string($value)) {
+      throw new MigrateException(sprintf('The value provided to subdelimited_explode must be a string; actual provided value: %s', var_export($value, TRUE)));
     }
 
     if ($value === '') {
       return [];
     }
 
-    // Establish some variables.
-    $limit = $this->configuration['limit'] ?? PHP_INT_MAX;
-    $sublimit = $this->configuration['sublimit'] ?? PHP_INT_MAX;
-    $keys = $this->configuration['keys'] ?? [];
-    $subdelimiter = $this->configuration['subdelimiter'];
 
     // Build and return the array. Resultant array should use keys from config;
     // if those run out, use the native index of each item.
-    $out = explode($this->configuration['delimiter'], $value, $limit);
-    array_walk($out, function (&$top_level) use ($sublimit, $subdelimiter, $keys) {
-      $top_level = explode($subdelimiter, $top_level, $sublimit);
-      foreach ($top_level as $idx => $piece) {
-        $key = $keys[$idx] ?? $idx;
-        $top_level[$key] = $piece;
-        if ($key !== $idx) {
-          unset($top_level[$idx]);
-        }
-      }
-    });
+    $out = explode($this->delimiter, $value, $this->limit);
+
+    // XXX: No need to trim at the top-level if subtrimming, as the subtrimming
+    // will account for it.
+    if (!$this->subtrim && $this->trim) {
+      $out = array_map('trim', $out);
+    }
+
+    array_walk($out, [$this, 'walker']);
+
     return $out;
+  }
+
+  /**
+   * Explode and key values according to configuration.
+   *
+   * @param mixed $top_level
+   *   A reference to a string to explode (turn into an array) and key.
+   */
+  protected function walker(&$top_level) {
+    $top_level = explode($this->subdelimiter, $top_level, $this->sublimit);
+
+    if ($this->subtrim) {
+      $top_level = array_map('trim', $top_level);
+    }
+
+    foreach ($top_level as $idx => $piece) {
+      $key = $this->keys[$idx] ?? $idx;
+      $top_level[$key] = $piece;
+      if ($key !== $idx) {
+        unset($top_level[$idx]);
+      }
+    }
   }
 
   /**
