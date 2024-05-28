a08460e53d316e24cbf745aaaac7a89a52c1a297
Immanuel Olisa <immanuelolisa@gmail.com>
2024-05-28 09:47:47 -0300

    Linting fixes

--- src/Util/MigrationRollbackBatch.php
+++ src/Util/MigrationRollbackBatch.php
@@ -2,11 +2,10 @@
 
 namespace Drupal\islandora_spreadsheet_ingest\Util;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\migrate\MigrateMessage;
 use Drupal\migrate\Plugin\MigrationInterface;
-use Drupal\migrate\Plugin\MigrationPluginManagerInterface;
-use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\migrate_tools\MigrateExecutable;
 
 /**
@@ -20,17 +19,16 @@ class MigrationRollbackBatch extends MigrateExecutable {
   }
 
   /**
-   * Manages migration plugin instances. It's used to create instances of migrations by their group ID.
+   * Messenger service to display messages.
    *
-   * @var MigrationPluginManagerInterface
+   * @var Drupal\Core\Messenger\MessengerInterface
    */
-  protected MigrationPluginManagerInterface $migrationPluginManager;
   protected MessengerInterface $messenger;
 
   public function __construct(
     MigrationInterface $migration,
     MessengerInterface $messenger,
-    array              $options,
+    array $options,
   ) {
     parent::__construct($migration, new MigrateMessage(), $options);
     $this->messenger = $messenger;
@@ -47,7 +45,10 @@ class MigrationRollbackBatch extends MigrateExecutable {
    */
   public function prepareBatch(): array {
     return [
-      'title' => $this->t('Rolling back migration: @migration', ['@migration' => $this->migration->id()]),
+      'title' => $this->t(
+          'Rolling back migration: @migration',
+          ['@migration' => $this->migration->id()]
+      ),
       'operations' => [
         [[$this, 'processBatch'], []],
       ],
@@ -56,37 +57,53 @@ class MigrationRollbackBatch extends MigrateExecutable {
   }
 
   /**
-   * Process each batch to roll back all contained rows
+   * Process each batch to roll back all contained rows.
    *
    * @param array $context
+   *   The batch context.
+   *
    * @return void
+   *   void
    */
   public function processBatch(array &$context): void {
-    $context['message'] = $this->t('Processing of "@migration_id"', ['@migration_id' => $this->migration->id()]);
+    $context['message'] = $this->t(
+        'Processing of "@migration_id"', ['@migration_id' => $this->migration->id()]);
 
     $status = $this->rollback();
 
     if ($status === MigrationInterface::RESULT_COMPLETED) {
-      $message = $this->t('Rollback completed', ['@id' => $this->migration->id()]);
+      $message = $this->t(
+          'Rollback completed',
+          ['@id' => $this->migration->id()]
+      );
       $this->messenger->addStatus($message);
     }
     else {
-      $message = $this->t('Rollback of @name migration failed.', ['@name' => $this->migration->id()]);
+      $message = $this->t(
+          'Rollback of @name migration failed.', ['@name' => $this->migration->id()]);
       $this->messenger->addError($message);
     }
 
-    $context['message'] = $this->t('"@migration_id" has been processed', ['@migration_id' => $this->migration->id()]);
+    $context['message'] = $this->t(
+        '"@migration_id" has been processed',
+        ['@migration_id' => $this->migration->id()]
+    );
   }
 
-
   /**
-   * Display success or error messages following the completion of processing
+   * Display success or error messages following the completion of processing.
    *
-   * @param $success
-   * @param $results
-   * @param $ops
-   * @param $interval
+   * @param bool $success
+   *   Indicates if the batch process was successful.
+   * @param array $results
+   *   The results of the batch process.
+   * @param array $ops
+   *   The operations performed.
+   * @param int $interval
+   *   The interval of the batch process.
+   *
    * @return void
+   *   void
    */
   public function finishBatch($success, $results, $ops, $interval): void {
     if (!$success || empty($results['errors'])) {
@@ -102,4 +119,5 @@ class MigrationRollbackBatch extends MigrateExecutable {
       }
     }
   }
+
 }
