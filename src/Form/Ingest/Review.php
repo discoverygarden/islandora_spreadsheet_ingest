adf1033040f4297eaea6b1b38ab6e74409b63209
Adam Vessey <adam@discoverygarden.ca>
2025-02-21 16:34:03 -0400

    Use the new method if present.

--- src/Form/Ingest/Review.php
+++ src/Form/Ingest/Review.php
@@ -145,6 +145,7 @@ class Review extends EntityForm {
    *   A reference to the batch context.
    */
   public function runBatchOp(MigrationInterface $migration, MigrateExecutable $e, &$context) {
+    assert($e instanceof MigrateBatchExecutable);
     $sandbox =& $context['sandbox'];
 
     if (!isset($sandbox['prepped'])) {
@@ -152,6 +153,11 @@ class Review extends EntityForm {
 
       try {
         $e->prepareBatch();
+        if (method_exists($e, 'getQueueSize')) {
+          // Method was introduced in dgi_migrate:3.16, the later use of
+          // $sandbox['total'] would have been broken since v3.
+          $sandbox['total'] = $e->getQueueSize();
+        }
       }
       catch (\Exception $ex) {
         $e->finishBatch(FALSE, [], [], NULL);
